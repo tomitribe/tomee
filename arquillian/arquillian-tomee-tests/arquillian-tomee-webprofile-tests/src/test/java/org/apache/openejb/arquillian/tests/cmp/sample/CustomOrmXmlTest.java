6d4d524b5a31aa0e3a9751eb66463cfc52cfdd15
Jonathan Gallimore <jon@jrg.me.uk>
2018-12-13 21:36:51 +0000

    Bump version number

--- arquillian/arquillian-tomee-tests/arquillian-tomee-webprofile-tests/src/test/java/org/apache/openejb/arquillian/tests/cmp/sample/CustomOrmXmlTest.java
+++ arquillian/arquillian-tomee-tests/arquillian-tomee-webprofile-tests/src/test/java/org/apache/openejb/arquillian/tests/cmp/sample/CustomOrmXmlTest.java
@@ -23,11 +23,13 @@ import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.ClassLoaderAsset;
+import org.jboss.shrinkwrap.api.exporter.ZipExporter;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.io.File;
 import java.net.URL;
 
 /**
@@ -53,6 +55,7 @@ public class CustomOrmXmlTest {
                 .addAsWebInfResource(new ClassLoaderAsset("org/apache/openejb/arquillian/tests/cmp/sample/web.xml"), "web.xml");
 
         System.out.println(archive.toString(true));
+        archive.as(ZipExporter.class).exportTo(new File("/Users/jgallimore/tmp/CustomOrmXmlTest.war"), true);
         return archive;
     }
 
