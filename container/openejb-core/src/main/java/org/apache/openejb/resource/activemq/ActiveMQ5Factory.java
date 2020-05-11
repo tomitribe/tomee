95865fd36689df281c38808aee3a866f7fdf0fd5
Jonathan Gallimore <jon@jrg.me.uk>
2020-05-11 11:22:35 +0100

    Set createConnector to false

--- container/openejb-core/src/main/java/org/apache/openejb/resource/activemq/ActiveMQ5Factory.java
+++ container/openejb-core/src/main/java/org/apache/openejb/resource/activemq/ActiveMQ5Factory.java
@@ -19,6 +19,7 @@ package org.apache.openejb.resource.activemq;
 import org.apache.activemq.broker.BrokerFactory;
 import org.apache.activemq.broker.BrokerFactoryHandler;
 import org.apache.activemq.broker.BrokerService;
+import org.apache.activemq.broker.jmx.ManagementContext;
 import org.apache.activemq.store.jdbc.JDBCPersistenceAdapter;
 import org.apache.activemq.store.memory.MemoryPersistenceAdapter;
 import org.apache.openejb.loader.SystemInstance;
@@ -127,6 +128,10 @@ public class ActiveMQ5Factory implements BrokerFactoryHandler {
             broker.setSystemExitOnShutdown(false);
 
             broker.setStartAsync(false);
+
+            final ManagementContext managementContext = new ManagementContext();
+            managementContext.setCreateConnector(false);
+            broker.setManagementContext(managementContext);
 
             final BrokerService bs = broker;
 
